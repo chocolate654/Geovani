a35e3fa857db21be1cb71f9cb82540a09d56f230
Geovani brasil <geobrasileoliveira11@gmail.com>
2025-04-26 12:29:34 -0300

    fiz a funçao verficaidade adolescente

--- main.c
+++ main.c
@@ -1,26 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void verificaIdadebebe(int anoAtual, int anoNascimento);
-int main()
+void verificaIdadeadolescente(int anoAtual, int anoNascimento)
 {
-    int anoAtual, anoNascimento;
-    printf("Digite seu ano de nascimento\n");
-    scanf("%d",&anoNascimento);
-    printf("Digite o ano atual\n");
-    scanf("%d",&anoAtual);
-
-    verificaIdadebebe(anoAtual,anoNascimento);
-
-    return 0;
-}
-
-
-void verificaIdadebebe(int anoAtual, int anoNascimento){
     int idade = anoAtual - anoNascimento;
-    if(idade>= 0 && idade<=3){
-        printf("Voce eh um bebe\n");
+    if(idade>= 11 && idade<=18)
+    {
+        printf("Voce eh um adolescente\n");
 
     }
-
 }
